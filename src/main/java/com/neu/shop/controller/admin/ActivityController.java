6c8e0b84189f07958ed145fbd8a677f8c50ddb6b
kuangsir <kuangbo_job@163.com>
2020-09-08 15:26:39 +0800

    test3333

--- src/main/java/com/neu/shop/controller/admin/ActivityController.java
+++ src/main/java/com/neu/shop/controller/admin/ActivityController.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpSession;
 import java.util.List;
 
 /**
- * Created by 文辉 on 2017/7/27.  ssm必须要掌握   111122
+ * Created by 文辉 on 2017/7/27.  ssm必须要掌握   333333
  */
 @Controller
 @RequestMapping("/admin/activity")
